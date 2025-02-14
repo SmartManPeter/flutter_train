8f370f55dbcb8b27173c9586d3499c65958557f0
peter <peter@smartman.com.tw>
2025-02-14 15:26:46 +0800

    初步架構參考

--- lib/main.dart
+++ lib/main.dart
@@ -12,9 +12,67 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello PeterSSSSSSSS!'),
+          child: Counter(defaultCount: 0),
         ),
       ),
+    );
+  }
+}
+
+class Counter extends StatefulWidget {
+  final int defaultCount;
+  //final Function({int Values}) TestFuns;
+  const Counter({super.key, required this.defaultCount});
+
+  @override
+  _CounterState createState() => _CounterState();
+}
+
+class _CounterState extends State<Counter> {
+  int Count = 0;
+
+  @override
+  void initState() {
+    Count = widget.defaultCount;
+    super.initState();
+  }
+
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          Count.toString(),
+          style: TextStyle(fontSize: 64),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            IconButton(
+              onPressed: () {
+                setState(() {
+                  Count++;
+                });
+              },
+              icon: Icon(
+                Icons.add,
+                size: 64,
+              ),
+            ),
+            IconButton(
+              onPressed: () {
+                setState(() {
+                  Count--;
+                });
+              },
+              icon: Icon(
+                Icons.remove,
+                size: 64,
+              ),
+            )
+          ],
+        )
+      ],
     );
   }
 }
